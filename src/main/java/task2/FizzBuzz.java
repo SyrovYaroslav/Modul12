388f6a6bb76ab3d985d7b55eb7a500b8796d96b9
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-10-31 15:24:27 +0300

    add task2.v3

--- src/main/java/task2/FizzBuzz.java
+++ src/main/java/task2/FizzBuzz.java
@@ -1,20 +1,71 @@
 package task2;
-
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
-public class FizzBuzz implements Runnable{
-    private int n;
-    private ArrayList<String> result = new ArrayList<>();
+public class FizzBuzz extends Thread{
+    private final Semaphore semaphore;
+    private final int n;
 
-    public FizzBuzz(int n, ArrayList<String> result) {
+    public FizzBuzz(int n, Semaphore semaphore) {
         this.n = n;
-        this.result = result;
+        this.semaphore = semaphore;
     }
 
-    @Override
-    public void run() {
-        if (n % 15 == 0) {
-            result.add("fizzbuzz");
+
+    public void fizz() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        for (int i = 1; i <= n; i++) {
+            if(i % 3 == 0) {
+                System.out.println("fizz");
+            }
+        }
+        semaphore.release();
     }
+
+    public void buzz() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 1; i <= n; i++) {
+            if(i % 5 == 0) {
+                System.out.println("buzz");
+            }
+        }
+        semaphore.release();
+    }
+
+    public void fizzbuzz() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 1; i <= n; i++) {
+            if(i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            }
+        }
+        semaphore.release();
+    }
+
+    public void number() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 1; i <= n; i++) {
+            if(i % 3 != 0 && i % 5 != 0) {
+                System.out.println(i);
+            }
+        }
+        semaphore.release();
+    }
+
 }
--- src/main/java/task2/Task2.java
+++ src/main/java/task2/Task2.java
@@ -1,9 +1,13 @@
 package task2;
 
+import java.util.concurrent.Semaphore;
+
 public class Task2 {
     public static void main(String[] args) {
-        FizzBuzzCheking fizzBuzz = new FizzBuzzCheking();
-        fizzBuzz.check(15);
-        fizzBuzz.printResult();
+        Semaphore sem = new Semaphore(4);
+        new FizzBuzz(15, sem).fizz();
+        new FizzBuzz(15, sem).buzz();
+        new FizzBuzz(15, sem).fizzbuzz();
+        new FizzBuzz(15, sem).number();
     }
 }
